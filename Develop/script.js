1a65edfb4716db15c03e726a2cfec3cb76f6feb8
Andrew Starr <astarr93@outlook.com>
2020-06-03 17:13:30 -0400

    Condensed Passoword Length Validation. Added User Selection Validation. Added Logic to generate and return a password based on user selection.

--- Develop/script.js
+++ Develop/script.js
@@ -10,58 +10,78 @@ function writePassword() {
 }
 
 function generatePassword() {
+  // Setting Variables
+  const lowercase = "abcdefghijklmnopqrstuvwxyz";
+  const uppercase = lowercase.toUpperCase();
+  const numerical = "0123456789";
+  const special = "!@#$%&*=+";
+  let charBank = "";
+  let result = "";
 
   // Ask user for desired password length
-  var pwLength = parseInt(
+  let pwLength = parseInt(
     prompt("How many characters should your password be?")
   );
 
-  // Checking validation of user input for password length
-
-  if (Number.isInteger(pwLength) == false || pwLength < 8 || pwLength > 128) {
-    console.log("Invalid Entry. Please enter a numerical value between 8 and 128");
+  // Validation of user input for password length
+  if (!pwLength || pwLength < 8 || pwLength > 128) {
     alert("Invalid Entry. Please enter a numerical value between 8 and 128");
     generatePassword();
   } else {
-    var Password = [];
-    Password.length = pwLength;
     console.log("You selected a password length of: " + pwLength);
+
+    // Ask user if password should include uppercase characters
+    let pwUpper = confirm(
+      "Include uppercase characters in your random password?"
+    );
+    console.log("Include uppercase characters?: " + pwUpper);
+
+    // Ask user if password should include lowercase characters
+    let pwLower = confirm(
+      "Include lowercase characters in your random password?"
+    );
+    console.log("Include lowercase characters?: " + pwLower);
+
+    // Ask user if password should include special characters
+    let pwSpecial = confirm(
+      "Include special characters in your random password?"
+    );
+    console.log("Include special characters?: " + pwSpecial);
+
+    // Ask user if password should include whole numbers
+    let pwNumber = confirm("Include whole numbers in your random password?");
+    console.log("Include whole numbers?: " + pwNumber);
+
+    // Validate at least one character type was selected
+    if (!pwUpper && !pwLower && !pwSpecial && !pwNumber) {
+      console.log("No character type was selected. At least one type must be selected to make a password")
+      alert("At lease one character type (uppercase, lowercase, special, numbers) must be selected to make a password")
+      generatePassword();
+    }
+
+    // Concat User Selections to charBank by checking for truthy
+    if (pwUpper) {
+      charBank += uppercase;
+    }
+    if (pwLower) {
+      charBank += lowercase;
+    }
+    if (pwSpecial) {
+      charBank += special;
+    }
+    if (pwNumber) {
+      charBank += numerical;
+    }
+
+    // Loop desired password length, picking a random character from charbank, and returning the result to function WritePassword()
+    for (let i = 0; i < pwLength; i++) {
+      const charBankArray = charBank.split("");
+      index = Math.floor(Math.random() * charBank.length);
+      result += charBankArray[index];
+    }
+    return result;
   }
-
-  // Ask user if password should include uppercase characters?
-  var pwUpper = confirm(
-    "Include uppercase characters in your random password?"
-  );
-  console.log("Include uppercase characters?: " + pwUpper);
-
-  // Ask user if password should include lowercase characters?
-  var pwLower = confirm(
-    "Include lowercase characters in your random password?"
-  );
-  console.log("Include lowercase characters?: " + pwLower);
-
-  // Ask user if password should include special characters?
-  var pwSpecial = confirm(
-    "Include special characters in your random password?"
-  );
-  console.log("Include special characters?: " + pwSpecial);
-
-  // Ask user if password should include whole numbers?
-  var pwNumber = confirm(
-    "Include whole numbers in your random password?"
-  );
-  console.log("Include whole numbers?: " + pwNumber);
-
-// If confirms are true, add to an array
-
-
-
 }
-
-// console.log(
-//   "When asked if the password generator should include uppercase letters in your password, you selected "
-//     // pwUpper
-// );
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
