eeacec193c4398e452519d95f4efbe472853a98b
Andrew Starr <astarr93@outlook.com>
2020-06-03 01:11:37 -0400

    Condensed valdiation on prompt for password length

--- Develop/script.js
+++ Develop/script.js
@@ -15,22 +15,36 @@ function generatePassword() {
     prompt("How many characters should your password be?")
   );
   var pwLength = parseInt(pwChoiceLength);
+
   // Checking validation of user input for password length
-  if (Number.isInteger(pwLength) == false) {
-    alert(
-      "Please enter a numerical value between 8 and 128 for your new password."
-    );
-    pwChoiceLength = prompt("How many characters should your password be?");
-  } else if (pwLength < 8 || pwLength > 128) {
-    alert(
-      "Please enter a numerical value between 8 and 128 for your new password."
-    );
-    pwChoiceLength = prompt("How many characters should your password be?");
+  if (Number.isInteger(pwLength) == false || pwLength < 8 || pwLength > 128) {
+    alert("Invalid Entry. Please enter a numerical value between 8 and 128");
+    pwLength = 0;
+    generatePassword();
+  } else {
+    var Password = [];
+    Password.length = pwLength;
+    console.log("You selected a password length of: " + pwLength);
   }
-   
-  console.log("You selected a password length of: " + pwLength);
 }
-// Ask if the customer
+
+// Ask user if password should include uppercase characters?
+
+//   var pwChoiceUpper = confirm(
+//     "Include uppercase characters in your random password? Type 'Yes' or 'No'"
+//   );
+//   if (pwChoiceUpper === false) {
+//     var pwUpper = false;
+
+//   } else {
+//     var pwUpper = true;
+//   }
+// }
+
+// console.log(
+//   "When asked if the password generator should include uppercase letters in your password, you selected "
+//     // pwUpper
+// );
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
